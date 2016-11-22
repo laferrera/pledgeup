a4b97831b9032fd1fbf299d0c8f0303301fdbeeb
laferrera <jason.laferrera@gmail.com>
2016-11-21 20:02:03 -0500

    WIP

---
+++ app/controllers/pledges_controller.rb
@@ -0,0 +1,74 @@
+class PledgesController < ApplicationController
+
+  # GET /pledges
+  # GET /pledges.json
+  def index
+    @pledges = Pledge.all
+  end
+
+  # GET /pledges/1
+  # GET /pledges/1.json
+  def show
+  end
+
+  # GET /pledges/new
+  def new
+    @pledge = Pledge.new
+  end
+
+  # GET /pledges/1/edit
+  def edit
+  end
+
+  # POST /pledges
+  # POST /pledges.json
+  def create
+    hash = {name: params[:name], email: params[:email], cause1:params[:cause1]}
+    @pledge = Pledge.new(hash)
+    respond_to do |format|
+      if @pledge.save
+        format.html { redirect_to @pledge, notice: 'Pledge was successfully created.' }
+        format.json { render :show, status: :created, location: @pledge }
+      else
+        format.html { render :new }
+        format.json { render json: @pledge.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /pledges/1
+  # PATCH/PUT /pledges/1.json
+  def update
+    respond_to do |format|
+      if @pledge.update(pledge_params)
+        format.html { redirect_to @pledge, notice: 'Pledge was successfully updated.' }
+        format.json { render :show, status: :ok, location: @pledge }
+      else
+        format.html { render :edit }
+        format.json { render json: @pledge.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /pledges/1
+  # DELETE /pledges/1.json
+  def destroy
+    @pledge.destroy
+    respond_to do |format|
+      format.html { redirect_to pledges_url, notice: 'Pledge was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pledge
+      @pledge = Pledge.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def pledge_params
+      # params.permit(:name, :email, :causes)
+      params.permit(:name, :email, :cause1)
+    end
+end
